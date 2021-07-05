19e098ef5d08237e3842accc3fe6bde12df96a26
yoshna <ymarwaha_be19@thapar.edu>
2021-07-05 05:48:23 +0530

    finalesttt

--- ui.js
+++ ui.js
@@ -1,32 +1,32 @@
-class UI{
-    constructor(){
-        this.profile= document.getElementById('profile');
-    }
+class UI {
+  constructor() {
+    this.profile = document.getElementById("profile");
+  }
 
-    showRepo(repos){
-        let output = '';
-        repos.forEach((repo)=> {
-            output += `
+  showRepo(repos) {
+    let output = "";
+    repos.forEach((repo) => {
+      output += `
             <div class="card card-body mb-2">
             <div class="row">
                 <div class="col-md-6">
                     <a href="${repo.html_url}" target="_blank">${repo.name}</a>
                 </div>
                 <div class="col-md-6">
-                    <span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
+                 <span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
                     <span class="badge bg-secondary">Watchers: ${repo.watchers_count}</span>
                     <span class="badge bg-success">Forks: ${repo.forks_count}</span>
                 </div>
             </div>
             </div>
             `;
-        })
+    });
 
-        document.querySelector('#repos').innerHTML=output;
-    }
+    document.querySelector("#repos").innerHTML = output;
+  }
 
-    showProfile(user){
-        this.profile.innerHTML = `
+  showProfile(user) {
+    this.profile.innerHTML = `
         <div class="card card-body-mb-3">
         <div class="row">
             <div class="col-md-3">
@@ -51,32 +51,28 @@ class UI{
         <h3 class="page-heading-mb-3">Latest Repos</h3>
         <div id="repos"></div>
         `;
-    }
+  }
 
-    showAlert(message,classname)
-    {
-        this.clearAlert();
-        const div = document.createElement('div');
-        div.className=classname;
-        div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.searchContainer');
-        const search = document.querySelector('.search');
-        container.insertBefore(div,search);
-        setTimeout(() =>{
-            this.clearAlert();
-        },3000);
-    }
+  showAlert(message, classname) {
+    this.clearAlert();
+    const div = document.createElement("div");
+    div.className = classname;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".searchContainer");
+    const search = document.querySelector(".search");
+    container.insertBefore(div, search);
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
 
-    clearAlert()
-    {
-        const curr = document.querySelector('.alert');
-        if(curr)
-        {
-            curr.remove();
-        } 
+  clearAlert() {
+    const curr = document.querySelector(".alert");
+    if (curr) {
+      curr.remove();
     }
-    clearProfile()
-    {
-        this.profile.innerHTML = '';
-    }
+  }
+  clearProfile() {
+    this.profile.innerHTML = "";
+  }
 }
